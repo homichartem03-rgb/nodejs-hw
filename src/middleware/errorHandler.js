baee45ff0156006cfc2f95129ff606dfe627203f
Artem <homichartem03@icloud.com>
2026-09-06 13:40:32 +0200

    Fix HttpError handling

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,13 +1,13 @@
-import createHttpError from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  if (err instanceof createHttpError.HttpError) {
+  if (err instanceof HttpError) {
     return res.status(err.status).json({
       message: err.message,
     });
   }
 
-  res.status(500).json({
+  return res.status(500).json({
     message: err.message || 'Internal Server Error',
   });
 };
