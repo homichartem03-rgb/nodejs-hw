e25b2c262438b4b88a595b0970103a91bf3007f4
Artem <homichartem03@icloud.com>
2026-09-06 13:17:20 +0200

    Add MongoDB notes CRUD

---
+++ src/db/connectMongoDB.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+export const connectMongoDB = async () => {
+  await mongoose.connect(process.env.MONGO_URL);
+
+  console.log('✅ MongoDB connection established successfully');
+};
