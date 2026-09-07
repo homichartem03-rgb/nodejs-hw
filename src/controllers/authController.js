02c26621c4835c5a7c8f601a0b7f4c29e360fb7e
Artem <homichartem03@icloud.com>
2026-09-07 15:25:40 +0200

    Fix auth session handling

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -65,6 +65,12 @@ export const refreshUserSession = async (req, res) => {
   }
 
   if (session.refreshTokenValidUntil < new Date()) {
+    await Session.deleteOne({ _id: session._id });
+
+    res.clearCookie('sessionId');
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+
     throw createHttpError(401, 'Session token expired');
   }
 
